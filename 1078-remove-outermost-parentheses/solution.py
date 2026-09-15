91be755fee9d80e80c87d3dfe490c6c23242c7e9
Sharvil Chavan <sharvildkte10@gmail.com>
2026-09-15 16:20:49 +0000

    Sync LeetCode submission Runtime - 4 ms (22.43%), Memory - 19.4 MB (30.97%)

---
+++ 1078-remove-outermost-parentheses/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        result = []
+        opened = 0
+        
+        for char in s:
+            if char == '(':
+                if opened > 0:
+                    result.append(char)
+                opened += 1
+            else:
+                opened -= 1
+                if opened > 0:
+                    result.append(char)
+                    
+        return "".join(result)
+
